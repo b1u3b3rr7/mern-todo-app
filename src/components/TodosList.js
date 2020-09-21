1c920d7ab56cfcc5d7145b0a23fcb635ef92d1ff
b1u3b3rr7 <berry31337@gmail.com>
2020-09-21 16:30:27 +0900

    chapter 3 finished

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Todo(props) {
+    const { _id, description, responsible, priority } = props.todo;
+    return (
+        <tr>
+            <td>{description}</td>
+            <td>{responsible}</td>
+            <td>{priority}</td>
+            <td>
+                <Link to={'/edit/' + _id}>Edit</Link>
+            </td>
+        </tr>
+    );
+}
 
 function TodosList() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/todos/')
+            .then(response => {
+                setTodos(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        return () => {
+            console.log("Component disappeared.");
+        }
+    }, []);
     return (
         <div>
-            <p>Welcome to Todos List Component!</p>
+            <h3>Todos List</h3>
+            <table className="table table-striped" style={{ marginTop: 20 }}>
+                <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th>Responsible</th>
+                        <th>Priority</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <Todo todo={todo} key={todo._id}></Todo>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
